053532046b23a83771647cfe3658bc7618bfa787
codeblech <my.malikyash@gmail.com>
2024-12-28 01:25:29 +0530

    docs fix2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,14 +14,14 @@ release = "0.0.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
-templates_path = ["_templates"]
-exclude_patterns = [
+extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
 ]
+
+templates_path = ["_templates"]
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
